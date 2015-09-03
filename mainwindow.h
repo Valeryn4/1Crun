1c61ea33902a2434013987504b007a9b5fc45225
Valeryn4 <dtho47@yandex.ru>
2015-09-03 21:47:59 +1000

    fix

--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QProcess>
-#include <QString>
-#include <QDebug>
-#include <QFile>
-#include <QDir>
+
 
 namespace Ui {
 class MainWindow;
@@ -17,30 +13,11 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 private:
     Ui::MainWindow *ui;
-
-    QList<QString> path;
-    QString shutdown;
-    QProcess * run;
-    QString getName(int index);
-    void run1C(int index);
-    void confPath();
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
-
 private slots:
-    void end_program();
-    void on_pushButton_exit_clicked();
-    void on_pushButton_1_clicked();
-    void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
-    void on_pushButton_4_clicked();
-    void on_pushButton_5_clicked();
-    void on_pushButton_6_clicked();
-    void on_pushButton_7_clicked();
-    void on_pushButton_8_clicked();
-    void on_pushButton_9_clicked();
+
 };
 
 #endif // MAINWINDOW_H
